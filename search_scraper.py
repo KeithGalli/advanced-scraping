763dd09f970c50895045e0f4f7c836f06e929308
Keith Galli <k.galli603@gmail.com>
2024-05-23 20:04:16 -0400

    Add fake headers and printing relevant info

--- search_scraper.py
+++ search_scraper.py
@@ -6,6 +6,15 @@ import queue
 BASE_URL = "https://www.walmart.com"
 OUTPUT_FILE = "product_info.jsonl"
 
+# Fake browser-like headers
+BASE_HEADERS = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    # "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "accept": "application/json",
+    "accept-language": "en-US",
+    "accept-encoding": "gzip, deflate, br, zstd",
+}
+
 # List of search queries
 search_queries = ["computers", "laptops", "desktops", "monitors", "printers", "hard+drives", "usb", "cords", "cameras", "mouse", "keyboard", "microphones", "speakers", "radio", "tablets", "android", "apple", "watch", "smart+watch"]
 
@@ -15,20 +24,25 @@ seen_urls = set()
 
 def get_product_links_from_search_page(query, page_number):
     search_url = f"https://www.walmart.com/search?q={query}&page={page_number}"
-    response = requests.get(search_url)
+    response = requests.get(search_url, headers=BASE_HEADERS)
     soup = BeautifulSoup(response.text, 'html.parser')
     product_links = []
 
     for a_tag in soup.find_all('a', href=True):
         if '/ip/' in a_tag['href']:
-            full_url = BASE_URL + a_tag['href']
+            if "https" in a_tag['href']:
+                full_url = a_tag['href']
+            else:
+                full_url = BASE_URL + a_tag['href']
+
             if full_url not in seen_urls:
                 product_links.append(full_url)
 
     return product_links
 
 def extract_product_info(product_url):
-    response = requests.get(product_url)
+    print("Processing URL", product_url)
+    response = requests.get(product_url, headers=BASE_HEADERS)
     soup = BeautifulSoup(response.text, 'html.parser')
     script_tag = soup.find('script', id='__NEXT_DATA__')
 
@@ -58,7 +72,7 @@ def main():
     with open(OUTPUT_FILE, 'w') as file:
         while search_queries:
             current_query = search_queries.pop(0)
-            print("CURRENT QUERY", current_query)
+            print("\n\nCURRENT QUERY", current_query, "\n\n")
             page_number = 1
 
             while True:
@@ -81,6 +95,7 @@ def main():
                         print(f"Failed to process URL: {product_url}. Error: {e}")
 
                 page_number += 1
+                print(page_number)
 
 if __name__ == "__main__":
     main()
